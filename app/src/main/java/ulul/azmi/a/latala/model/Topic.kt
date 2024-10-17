b22233760be9a9f1abee0669f81a2be585c57731
Ulul Azmi A. Latala <133023139+UlulAzmiALatala@users.noreply.github.com>
2024-10-18 01:17:02 +0700

    Tugas Pertemuan 5

--- app/src/main/java/ulul/azmi/a/latala/model/Topic.kt
+++ app/src/main/java/ulul/azmi/a/latala/model/Topic.kt
@@ -3,8 +3,9 @@ package ulul.azmi.a.latala.model
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
+// Data class untuk mendefinisikan model 'Topic'
 data class Topic(
-    @StringRes val name: Int,
-    val availableCourses: Int,
-    @DrawableRes val imageRes: Int
+    @StringRes val name: Int,              // ID untuk string resource (nama topik)
+    val availableCourses: Int,             // Jumlah kursus yang tersedia dalam topik ini
+    @DrawableRes val imageRes: Int          // ID untuk drawable resource (gambar topik)
 )
