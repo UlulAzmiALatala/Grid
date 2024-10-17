de60fd20382fb210b23265d1ed07d3089c076520
UlulAzmiALatala <ullulazmia.l@gmail.com>
2024-10-18 00:49:38 +0700

    Tugas Pertemuan 5

---
+++ app/src/main/java/ulul/azmi/a/latala/model/Topic.kt
@@ -0,0 +1,10 @@
+package ulul.azmi.a.latala.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
+)
